5253e33f5b70befa1862b60f449790aca1ff4641
OlivierBovier <olivier.bovier@free.fr>
2019-05-08 10:58:42 +0200

    mise en place des assets

--- src/Controller/MastermindController.php
+++ src/Controller/MastermindController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class MastermindController extends AbstractController
@@ -15,21 +17,19 @@ class MastermindController extends AbstractController
 	function initGame () {
 		$row = 1;
 		$expertMode = 0; // 1 pour activer le mode expert (la même couleur peut être utilisée plusieurs fois dans la combinaison)
-		$combinaison = createCombinaison(4);
+		$combinaison = $this->createCombinaison(4);
 
 		return $this->render('init.html.twig', ['combinaison' => $combinaison]);
 
 	}
 
 	function createCombinaison ($nbPosition) {
-		$availableColors = [$blanc => '#FFFFFF', $jaune => '#FFF933', $orange => '#FF5733', $rouge => '#FF0000', $bleu => '#0000FF', $vert => '#00FF00', $gris => '#808080', $rose => '#FF00FF'];
+		$availableColors = ['blanc' => '#FFFFFF', 'jaune' => '#FFF933', 'orange' => '#FF5733', 'rouge' => '#FF0000', 'bleu' => '#0000FF', 'vert' => '#00FF00', 'gris' => '#808080', 'rose' => '#FF00FF'];
 		$positionsColor = [];
 		for ($i = 1; $i <= $nbPosition; $i++) {
-    		$positionsColor[$i] = array_rand($availableColors);
+    		$positionsColor[$i] = $availableColors[array_rand($availableColors)];
 		}
-		var_dump($positionsColor);die();
 		return $positionsColor;
-
 	}
 
 	function checkRow () {
@@ -39,8 +39,3 @@ class MastermindController extends AbstractController
 
 }
 
-
-
-
-
-  ?>
