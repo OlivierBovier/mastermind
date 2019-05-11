0f053f601352b85d9c392dc74e304889713f058f
OlivierBovier <olivier.bovier@free.fr>
2019-05-11 11:03:00 +0200

    formulaire-phase-2

--- src/Controller/MastermindController.php
+++ src/Controller/MastermindController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 
 class MastermindController extends AbstractController
 {
@@ -18,40 +19,43 @@ class MastermindController extends AbstractController
 	*/
 	function homepage (Request $request) {
 		
-		$game = new Game();
+		// $game = new Game();
 
-	    $form = $this->createFormBuilder($game)
-	        ->add('expertmode', TextType::class)
-	        ->add('numberofcolours', DateType::class)
+	    $form = $this->createFormBuilder()
+	        ->add('expertmode', CheckboxType::class, [
+    			'label'    => 'Mode Expert(la même couleur peut être présente plusieurs fois dans la combinaison)',
+    			'required' => false,
+				])
+	        ->add('numberofcolours', ChoiceType::class, [
+    			'choices'  => [
+       			 	'4' => 4,
+        			'5' => 5,
+        			'5' => 6,],
+    			// *this line is important*
+    			'choice_value' => '4',
+    			'label'    => 'Nombre de positions à deviner (4, 5 ou 6)',
+				])
 	        ->add('save', SubmitType::class, ['label' => 'Nouveau jeu'])
 	        ->getForm();
 
 	    $form->handleRequest($request);
 
 	    if ($form->isSubmitted() && $form->isValid()) {
-	        // $form->getData() holds the submitted values
-	        // but, the original `$task` variable has also been updated
-	        $task = $form->getData();
-
-	        // ... perform some action, such as saving the task to the database
-	        // for example, if Task is a Doctrine entity, save it!
-	        // $entityManager = $this->getDoctrine()->getManager();
-	        // $entityManager->persist($task);
-	        // $entityManager->flush();
+	        $param = $form->getData();
+	        $this->iniGame($param);
 
 	        return $this->redirectToRoute('task_success');
+	        return $this->render('game.html.twig', ['combinaison' => $combinaison]);
 	    }
 
-	    return $this->render('task/new.html.twig', [
+	    return $this->render('init.html.twig', [
 	        'form' => $form->createView(),
-	    ]);
-
-		return $this->render('game.html.twig', ['combinaison' => $combinaison]);
+	    ]);		
 
 	}
 
 
-	function initGame () {
+	function initGame ($param) {
 		$row = 1;
 		$expertMode = 0; // 1 pour activer le mode expert (la même couleur peut être utilisée plusieurs fois dans la combinaison)
 		$numberOfColoursToFind = 5;
